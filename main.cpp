07d58d7019dea0fc2b34536115d9901befdb55fd
laura.zuluagaj <laura.zuluagaj@udea.edu.co>
2020-10-22 09:09:09 -0500

    Prueba rama

--- main.cpp
+++ main.cpp
@@ -19,6 +19,13 @@ int main()
     for (int var = 0; var < TAM; ++var){
         cout << a[var] << endl;
     }
-    cout << "Hello World!" << endl;
+
+    cout << "Imprimir solo los numeros de la cadena orignal" << endl;
+
+    for(int var = 0;var < TAM; ++var){
+        if (a[var]>='0' || a[var]<='9'){
+            cout << a[var] << endl;
+        }
+    }
     return 0;
 }
