84c29771f68a9552dfc762c5164f0fbcc18c7b05
laura.zuluagaj <laura.zuluagaj@udea.edu.co>
2020-10-22 09:18:31 -0500

    Impresion de los numeros de la cadena de caracteres original

--- main.cpp
+++ main.cpp
@@ -8,22 +8,24 @@ izquierda del primer numero.*/
 #include <iostream>
 
 //Variables globales y macros
-#define TAM (int)10
+#define TAM (int)50
 
 using namespace std;
 
 int main()
 {
-    char a[TAM]="AB11CD44";
+    char a[TAM]="AB11CD44EF5X5";
 
     for (int var = 0; var < TAM; ++var){
-        cout << a[var] << endl;
+        if(a[var]!='\0'){
+            cout << a[var] << endl;
+        }
     }
 
     cout << "Imprimir solo los numeros de la cadena orignal" << endl;
 
     for(int var = 0;var < TAM; ++var){
-        if (a[var]>='0' || a[var]<='9'){
+        if (a[var]>='0' && a[var]<='9'){
             cout << a[var] << endl;
         }
     }
